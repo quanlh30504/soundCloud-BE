937387445390a3a02e485b34958c8f9f89c69306
Quân Nguyễn <quanlh3052004@gmail.com>
2025-04-05 08:10:48 +0100

    minor

--- src/main/java/com/example/soundCloud_BE/config/FirebaseConfig.java
+++ src/main/java/com/example/soundCloud_BE/config/FirebaseConfig.java
@@ -2,18 +2,25 @@ package com.example.soundCloud_BE.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.credentials.path}")
+    private String firebaseCredentialsPath;
+
     @PostConstruct
     public void initialize() {
         try {
-            InputStream serviceAccount = getClass().getResourceAsStream("/firebase-service-account.json");
+//            InputStream serviceAccount = getClass().getResourceAsStream("/firebase-service-account.json");
+            FileInputStream serviceAccount = new FileInputStream(firebaseCredentialsPath);
+
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
